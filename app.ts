14f226798d7da0236988b16793cd62b9ea7d66dc
Oliver Skjevesland <oliver@skjevesland.com>
2022-02-14 00:53:31 +0100

    added styling and words array

---
+++ app.ts
@@ -0,0 +1,14 @@
+const words = [
+    "four", "able", "aged",
+    "army", "bank", "band",
+    "base", "boat", "king", 
+    "acid", "bird", "burn", 
+    "camp", "bone", "down", 
+    "earn", "gift"
+]
+
+const randomWord = words[Math.floor(Math.random() * words.length)]
+
+let row1letter1 = document.getElementById("row1letter")
+
+console.log(row1letter1)
